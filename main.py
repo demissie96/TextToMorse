905bd1592ab8c6869661642afa5c9b7bfc56c651
Johannes Demissie <demissie.johannes@gmail.com>
2022-08-16 14:01:28 +0200

    Change input text to morse code

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import json
 
-print('********************')
-
 f = open('./morse-code.json')
 morse_file = json.load(f)
-print(json.dumps(morse_file, indent=4, sort_keys=False))
 
+print('\nLetters are separated by spaces and words by "/". If a letter cannot be translated a "#" will appear in the output.\n')
+user_input = input('Type the text you want to convert into morse code: ').lower()
 
+text_in_morse = ''
 
+for letter in user_input:
+    try: 
+        text_in_morse += morse_file[letter] + ' '
+    except:
+        text_in_morse += '# '
 
+print('The answer is: ' + text_in_morse)
 
-print('********************')
