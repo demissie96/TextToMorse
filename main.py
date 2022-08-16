52f6e5b3129becee3ebf712bdbc345274dad5641
Johannes Demissie <demissie.johannes@gmail.com>
2022-08-16 14:30:57 +0200

    Add while loop to keep the app running

--- main.py
+++ main.py
@@ -3,16 +3,28 @@ import json
 f = open('./morse-code.json')
 morse_file = json.load(f)
 
-print('\nLetters are separated by spaces and words by "/". If a letter cannot be translated a "#" will appear in the output.\n')
-user_input = input('Type the text you want to convert into morse code: ').lower()
+print('\nLetters are separated by spaces and words by "/". If a letter cannot be translated a "#" will appear in the output.')
 
-text_in_morse = ''
+machine_is_on = True
 
-for letter in user_input:
-    try: 
-        text_in_morse += morse_file[letter] + ' '
-    except:
-        text_in_morse += '# '
+while machine_is_on:
 
-print('The answer is: ' + text_in_morse)
+    
+    user_input = input('\nType the text you want to convert into morse code: ').lower()
+
+    text_in_morse = ''
+
+    for letter in user_input:
+        try: 
+            text_in_morse += morse_file[letter] + ' '
+        except:
+            text_in_morse += '# '
+
+    print('\nThe answer is: ' + text_in_morse)
+
+    want_to_continue = input("\nDo you want to continue? Type 'yes' to continue or type 'no' to exit: ").lower()
+
+    if want_to_continue != 'yes':
+        machine_is_on = False
+
 
